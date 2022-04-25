73e8bb29dbf9fd0b03f9d7b95aad193988235415
庞云川 <pangyunchuan@bonc.com.cn>
2022-04-25 11:37:08 +0800

    增加配置项以支持压缩包含dist目录本身,修复说明文件问题;

--- README.md
+++ README.md
@@ -34,5 +34,5 @@ export default defineConfig({
 |  ----| ---- | ---- | ----                             |
 | zipName | 字符串 | dist  | 生成的压缩包名称 |
 | zipDir | 字符串 |   | 压缩文件存放路径;相对于vite项目根目录;为空时值为 viteConfig.build.outDir;不会创建目录,请确保目录已存在 |
-| zipName | 字符串 | dist  |  dayjs 日期格式 YYMMDD_HHmm,会拼在压缩包名称上,便于区分压缩包 |
-
+| dayjsFormat | 字符串 | YYMMDD_HHmm  |  dayjs 日期格式 YYMMDD_HHmm,会拼在压缩包名称上,便于区分压缩包 |
+| includeDistDir | 布尔 | false  |  为true时  压缩包会包含dist 目录本身,  false 只压缩dist 内容 |
--- lib/index.ts
+++ lib/index.ts
@@ -12,9 +12,10 @@ const nowDateTime = dayjs();
  * @param zipName 默认值 "dist";  文件名称,不包含后缀名
  * @param zipDir  默认值 ""; 压缩文件存放路径;相对于vite项目根目录;默认空且为空时值为 viteConfig.build.outDir;不会创建目录,请确保目录已存在
  * @param dayjsFormat 默认值 "YYMMDD_HHmm" ; dayjs 日期格式  YYMMDD_HHmm,会拼在压缩包名称上,便于区分压缩包
+ * @param includeDistDir 默认值 false ; 为true时  压缩包会包含dist 目录本身,  false 只压缩dist 内容
  */
 export default function (
-    {zipName = "dist", zipDir = "", dayjsFormat = "YYMMDD_HHmm"} = {}
+    {zipName = "dist", zipDir = "", dayjsFormat = "YYMMDD_HHmm",  includeDistDir = false} = {}
 ): Plugin {
     let viteConfig: ResolvedConfig;
     return <Plugin>{
@@ -51,7 +52,12 @@ export default function (
                 });
             }
 
-            readDir(zip, distDir);
+            let folder = null;
+            if (includeDistDir) {
+                folder = zip.folder(path.basename(distDir))
+            }
+
+            readDir(folder || zip, distDir);
 
             zip.generateAsync({
                 type: "nodebuffer", // 压缩类型
