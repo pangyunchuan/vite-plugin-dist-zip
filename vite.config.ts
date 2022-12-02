f7d9634cf75d1958a964325d7bdaaf6e631b38ae
zhuxianyong <zhuxianyong@baidu.com>
2022-12-02 18:55:03 +0800

    fix: 修复打包后es模块找不到fs,path的问题

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             // 确保外部化处理那些你不想打包进库的依赖
-            external: ['dayjs', 'jszip'],
+            external: ['dayjs', 'jszip', 'fs', 'path'],
             output: {
                 // // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
                 globals: {
